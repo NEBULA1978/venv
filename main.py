a59a93ac83b34423b723766aeb19dbef72770359
NEBULA1978 <rpascualey@gmail.com>
2023-06-05 00:17:50 +0200

    Segunda forma htmlcon fastapi por navegadorsin css

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 
 app=FastAPI()
 
+# Ruta para archivos html
 @app.get("/",response_class=HTMLResponse)
 def root():
-    return """
-    <html>
-        <head>
-            <title>Web FastAPI</title>
-        </head>
-        <body>
-            <h1>Hi I a, a html file</h1>
-        </body>
-    </html>
-
-    """
+    html_address = "./public/html/index.html"
+    return FileResponse(html_address,status_code=200)
