ea5f042afff8f749191795ffc22f2769d14fc52b
NEBULA1978 <rpascualey@gmail.com>
2023-06-05 00:52:35 +0200

    Segunda forma html con fastapi por navegadorsin css creamdo carpeta static funcionando

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app=FastAPI()
+
+app.mount("/static",StaticFiles(directory="./public/static"),name="static")
 
 # Ruta para archivos html
 @app.get("/",response_class=HTMLResponse)
 def root():
-    html_address = "./public/html/index.html"
+    html_address = "./public/static/index.html"
     return FileResponse(html_address,status_code=200)
