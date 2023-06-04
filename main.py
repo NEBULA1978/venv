4c479dd55f87950f9c5e7b492fc0c9f90ce3add6
NEBULA1978 <rpascualey@gmail.com>
2023-06-05 00:05:11 +0200

    Primera forma htmlcon fastapi por navegador

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app=FastAPI()
 
-@app.get("/")
+@app.get("/",response_class=HTMLResponse)
 def root():
-    return "Hi,i am Fastapi"
+    return """
+    <html>
+        <head>
+            <title>Web FastAPI</title>
+        </head>
+        <body>
+            <h1>Hi I a, a html file</h1>
+        </body>
+    </html>
+
+    """
