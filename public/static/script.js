68383a6a61cfdb174ac27b1e1d2af61e6bf312c8
NEBULA1978 <rpascualey@gmail.com>
2023-07-24 00:57:36 +0200

    Añadiendo web basica,añado espacio en blanco al final

--- public/static/script.js
+++ public/static/script.js
@@ -1,1 +1,18 @@
 alert("Hi, i am a script")
+
+document.addEventListener("DOMContentLoaded", function () {
+  const divContainer = document.createElement("div");
+  divContainer.style.display = "flex";
+  divContainer.style.justifyContent = "center";
+  divContainer.style.alignItems = "center";
+  divContainer.style.height = "100vh";
+
+  const button = document.createElement("button");
+  button.textContent = "Iniciar";
+  button.onclick = function () {
+    window.location.href = "index.html";
+  };
+
+  divContainer.appendChild(button);
+  document.body.insertBefore(divContainer, document.body.firstChild);
+});
